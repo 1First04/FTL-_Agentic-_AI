83fa0d86f5fdbf80ccc97c26c4d255993947c730
1First04 <hunda4863@gmail.com>
2026-01-16 12:46:01 -0800

    Add product list for Android phones

---
+++ Tasks/Implementing_Agentic_RAG.py
@@ -0,0 +1,16 @@
+products = [
+    {
+        "name": "Samsung Galaxy A14",
+        "price": 220,
+        "battery": "5000mAh",
+        "safety": "Certified",
+        "category": "Android Phone"
+    },
+    {
+        "name": "Xiaomi Redmi Note 12",
+        "price": 280,
+        "battery": "5000mAh",
+        "safety": "Certified",
+        "category": "Android Phone"
+    }
+]
